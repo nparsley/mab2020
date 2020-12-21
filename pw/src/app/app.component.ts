dbd4437726d97696421699dbd3e324b17f4bddb6
nparsley <nickparsley5@gmail.com>
2020-12-21 15:54:52 -0700

    interpolation + tracking input state & properties

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -7,8 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+
+  letterChange() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  numberChange() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  symbolChange() {
+    this.includeSymbols = !this.includeSymbols;
+  }
 
   onButtonClick() {
+    // console.log(this.includeLetters);
+    // console.log(this.includeNumbers);
+    // console.log(this.includeSymbols);
+    console.log(`
+    About to generate a password with the following:
+    Includes letters: ${this.includeLetters}
+    Includes numbers: ${this.includeNumbers}
+    Includes symbols: ${this.includeSymbols}
+    `)
     this.password = 'MY PASSWORD';
   }
+
+
 }
