98ed5d96b2ce1cd6499b4ffb2d7f2c35593adf6a
nparsley <nickparsley5@gmail.com>
2020-12-28 22:30:55 -0700

    character, styling comparison

--- typing/src/app/app.component.ts
+++ typing/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { spawn } from 'child_process';
 import { lorem } from 'faker';
 
 
@@ -14,7 +15,14 @@ export class AppComponent {
 
   onInput(value: string) { // without type info
     this.enteredText = value;
+  }
 
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
   }
 
 }
