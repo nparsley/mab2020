e4f412d89492e01600243f01b35bfebe59ffc4dd
nparsley <nickparsley5@gmail.com>
2020-12-30 17:33:07 -0700

    image + add data

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+  images = [
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1489573280374-2e193c63726c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8ZGVzZXJ0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1586957469525-7850e7bef283?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGp1bmdsZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1535914728398-fcc06686536c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8bW9uc29vbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    }
+  ];
 }
