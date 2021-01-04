61068b6d5b4d03f81331b438057b9031e1863a5a
nparsley <nickparsley5@gmail.com>
2021-01-03 20:16:22 -0700

    collections table-generate,display data

--- comps/src/app/collections/collections.module.ts
+++ comps/src/app/collections/collections.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { TableComponent } from './table/table.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [CollectionsHomeComponent],
+  declarations: [CollectionsHomeComponent, TableComponent],
   imports: [
     CommonModule,
-    CollectionsRoutingModule
+    CollectionsRoutingModule,
+    SharedModule
   ],
   exports: [
-    
+
   ]
 })
 export class CollectionsModule { }
