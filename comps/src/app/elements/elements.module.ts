429799bed33b411a6543c33843f724026ec10fbf
nparsley <nickparsley5@gmail.com>
2021-01-03 18:37:36 -0700

    widget module + title divider

--- comps/src/app/elements/elements.module.ts
+++ comps/src/app/elements/elements.module.ts
@@ -5,16 +5,18 @@ import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { TimesDirective } from './times.directive';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule
   ],
   exports: [
-    
+
   ]
 })
 export class ElementsModule { }
