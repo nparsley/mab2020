458aa2c0882fa041e6cc082b0eb63863d6671db7
nparsley <nickparsley5@gmail.com>
2021-01-05 18:34:17 -0700

    items list component add + statistic,item display

--- comps/src/app/views/views.module.ts
+++ comps/src/app/views/views.module.ts
@@ -6,10 +6,11 @@ import { ViewsComponent } from './views.component';
 import { SharedModule } from '../shared/shared.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { StatisticsComponent } from './statistics/statistics.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
 
 @NgModule({
-  declarations: [ViewsComponent, ViewsHomeComponent, StatisticsComponent],
+  declarations: [ViewsComponent, ViewsHomeComponent, StatisticsComponent, ItemListComponent],
   imports: [
     CommonModule,
     ViewsRoutingModule,
