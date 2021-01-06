276fe87b363900429b27a76771ddc3e72a9192b6
nparsley <nickparsley5@gmail.com>
2021-01-05 18:09:56 -0700

    views module + viewshome,statistics component setup

--- comps/src/app/app.module.ts
+++ comps/src/app/app.module.ts
@@ -7,11 +7,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NotFoundComponent
+    NotFoundComponent,
+
   ],
   imports: [
     BrowserModule,
--- comps/src/app/views/views.module.ts
+++ comps/src/app/views/views.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
+import { ViewsComponent } from './views.component';
+import { SharedModule } from '../shared/shared.module';
+import { ViewsHomeComponent } from './views-home/views-home.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ViewsComponent, ViewsHomeComponent, StatisticsComponent],
   imports: [
     CommonModule,
-    ViewsRoutingModule
+    ViewsRoutingModule,
+    SharedModule
   ]
 })
 export class ViewsModule { }
