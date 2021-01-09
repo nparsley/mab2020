54bee329ce93f87ad13bf70758cc4d7e512f3e6f
nparsley <nickparsley5@gmail.com>
2021-01-08 19:04:58 -0700

    interface and classes + class generics

--- ts/index.ts
+++ ts/index.ts
@@ -1,12 +1,51 @@
-// const Component = (target: any) => {
-//     console.log(target);
-// };
-
-// @Component
-// class Car {
-
+// interface Driveable {
+//     speed: number;
+//     drive(): string;
 // }
 
-import { Car } from './Car';
+// class Car implements Driveable {
+//     speed = 10;
 
-const myCar = new Car();
+//     drive() {
+//        return (`i am driving at ${this.speed}`);
+//     }
+// }
+
+// const myCar = new Car();
+
+// const startDriving = (vehicle: Driveable) => {
+//     vehicle.drive();
+// };
+
+// startDriving(myCar);
+
+
+class NumberHolder {
+    value: number;
+}
+
+class StringHolder {
+    value: string;
+}
+
+class BooleanHolder {
+    value: boolean;
+}
+
+// const numberHolder = new NumberHolder();
+// numberHolder.value = 10;
+
+// const stringHolder = new StringHolder();
+// stringHolder.value = 'A string';
+
+
+class ValueHolder<TypeForValueProperty> {
+    value: TypeForValueProperty;
+}
+
+const numberHolder = new ValueHolder<number>();
+numberHolder.value;
+
+new ValueHolder<string>();
+new ValueHolder<boolean>();
+
