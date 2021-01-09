9a7eb2e5e23b0d9887585e556c4f8386a1badba8
nparsley <nickparsley5@gmail.com>
2021-01-08 17:40:19 -0700

    decorators + strict mode

--- ts/index.ts
+++ ts/index.ts
@@ -1,37 +1,12 @@
-class Car {
-    // color: string;
-    // private year: number; /* adding public and private in constructor below replaces this part */
+// const Component = (target: any) => {
+//     console.log(target);
+// };
 
-    constructor(public color: string, private year: number) {
-        // this.color = color; /* adding public and private in constructor above replaces this part */
-        // this.year = year;
-    }
+// @Component
+// class Car {
 
-    drive() {
-        this.putInGear();
-        this.pressPedal();
-        this.turnWheel();
-    }
+// }
 
-    private putInGear() {
-        
-    }
+import { Car } from './Car';
 
-    private pressPedal() {
-
-    }
-
-    private turnWheel() {
-
-    }
-
-}
-
-const myCar = new Car('red', 2000);
-// myCar.drive();
-
-// myCar.putInGear();
-
-// console.log(myCar.color, myCar.year);
-
-console.log(myCar.color);
+const myCar = new Car();
