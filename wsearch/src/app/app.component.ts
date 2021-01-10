ef3e08f08a162fa1597e27f84af06b06253e1ad0
nparsley <nickparsley5@gmail.com>
2021-01-09 20:12:02 -0700

    searchbar-form submission

--- wsearch/src/app/app.component.ts
+++ wsearch/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wsearch';
+  onTerm(term: string) {
+    console.log('i am the app and her is the term,', term);
+  }
 }
