54209bcd37b5d297a8f996cffb297f91e3349803
nparsley <nickparsley5@gmail.com>
2021-02-02 16:13:35 -0700

    modal-compose,edit + emailform component setup

--- emailClient/src/app/inbox/email-create/email-create.component.ts
+++ emailClient/src/app/inbox/email-create/email-create.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-create.component.css']
 })
 export class EmailCreateComponent implements OnInit {
+  showModal = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
