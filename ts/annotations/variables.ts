7dfc2e5eda13ffe7a28c8fb20810a20df8b56875
nparsley <nickparsley5@gmail.com>
2021-02-06 20:33:37 -0700

    ts-any,delayed init

--- ts/annotations/variables.ts
+++ ts/annotations/variables.ts
@@ -32,3 +32,18 @@ let point: { x: number; y: number } = {
 const  logNumber: (i: number) => void = (i: number) => {
     console.log(i);
 };
+
+// when to use annotations
+// 1) function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20};
+// 2) when we declare a variable on one line and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+        foundWord = true;
+    }
+}
