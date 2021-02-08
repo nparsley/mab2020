a15a89e6651e4180f131064272e5afcf8ce3f437
nparsley <nickparsley5@gmail.com>
2021-02-07 18:22:41 -0700

    ts-annotations functions + void

--- ts/annotations/functions.ts
+++ ts/annotations/functions.ts
@@ -1,3 +1,28 @@
 const add = (a: number, b: number): number => {
     return a + b;
 };
+
+const subtract = (a: number, b: number): number => {
+    return a - b;
+};
+
+function divide(a: number, b: number): number {
+    return a / b;
+};
+
+const multiply = function(a: number, b: number): number {
+    return a * b;
+};
+
+const logger = (message: string): void => {
+    console.log(message);
+    return null;
+    return undefined;
+};
+
+const throwError = (message: string): void => {
+    if (!message) {
+        throw new Error(message);
+    }
+    // return message;
+};
