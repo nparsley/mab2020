72ea270d31cd883e9d25ab2c332068832ddd50fc
nparsley <nickparsley5@gmail.com>
2021-02-09 16:03:39 -0700

    ts-interfaces,functions

--- ts/annotations/interfaces.ts
+++ ts/annotations/interfaces.ts
@@ -1,24 +1,42 @@
 
-interface Vehicle {
-    name: string;
-    year: number;
-    broken: boolean;
+// interface Vehicle {
+    interface Reportable {
+    // name: string;
+    // year: Date;
+    // broken: boolean;
+    summary(): string;
 
 }
 
 const oldCivic = {
     name: 'civic',
-    year: 2000,
-    broken: true
+    year: new Date(),
+    broken: true,
+    summary(): string {
+        return `Name: ${this.name}`;
+    }
+};
+
+const drinks = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40,
+    summary(): string {
+        return `My drink has ${this.sugar} grams of sugar`;
+    }
+
 };
 
 
 
 // const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
-    const printVehicle = (vehicle: Vehicle): void => {
-    console.log(`Name: ${vehicle.name}`);
+    const printSummary = (item: Reportable): void => {
+/*     console.log(`Name: ${vehicle.name}`);
     console.log(`Year: ${vehicle.year}`);
-    console.log(`Broken: ${vehicle.broken}`);
+    console.log(`Broken: ${vehicle.broken}`); */
+
+    console.log(item.summary());
 };
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
+printSummary(drinks);
