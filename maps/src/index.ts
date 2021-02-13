be5619945e0a537e511f262a758d965a4adf8e49
nparsley <nickparsley5@gmail.com>
2021-02-13 01:12:11 -0700

    custommap-markers,functionality

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,13 +1,12 @@
-// import { User } from './User';
-// import { Company } from './Company';
+import { User } from './User';
+import { Company } from './Company';
+
+import { CustomMap } from './CustomMap';
 
 
-const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-});
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
 
-map
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
