37c6ad5e097aa0f9d6c2cb865cc62c7e1ee81e0b
nparsley <nickparsley5@gmail.com>
2021-02-16 15:27:03 -0700

    Sorter setup-extract logic key

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,37 +1,4 @@
-class Sorter {
-    // collection: number[];
 
-    constructor(public collection: number[] | string) {
-        // this. collection = collection;
-    }
-
-    sort(): void {
-        const { length } = this.collection;
-
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length -i - 1; j++) {
-
-                // all of this only works if collection is number []
-                // if collection is an array of numbers
-                if (this.collection instanceof Array) {
-                    //collection ===  number[]
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-
-                // only work if collection is a string
-                // if collection is a string, do this logic instead
-                // logic to compare and swap characters in a string
-                if (typeof this.collection === 'string') {
-                    
-                }
-            }
-        }
-    }
-}
 
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
