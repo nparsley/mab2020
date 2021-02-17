b2ce0e84d613abc793ca1a77b2e4c931440141aa
nparsley <nickparsley5@gmail.com>
2021-02-17 13:07:26 -0700

    characterscollection-sorter

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,24 +1,12 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    // collection: number[];
-    function Sorter(collection) {
-        this.collection = collection;
-        // this. collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
-var sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+var CharactersCollection_1 = require("./CharactersCollection");
+var Sorter_1 = require("./Sorter");
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+var charactersCollection = new CharactersCollection_1.CharactersCollection('xaayb');
+var sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(charactersCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,12 @@
+import { CharactersCollection } from "./CharactersCollection";
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+ const charactersCollection = new CharactersCollection('xaayb');
+ const sorter = new Sorter(charactersCollection);
+ sorter.sort();
+ console.log(charactersCollection.data);
