ed8710492c8c28390a54e2b162f806accc4ed044
nparsley <nickparsley5@gmail.com>
2021-02-20 16:27:40 -0700

    module connect + geolocation coords add

--- landing/src/app/weather/forecast.service.ts
+++ landing/src/app/weather/forecast.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class WeatherService {
+export class ForecastService {
 
   constructor() { }
+
+  getCurrentLocation() {
+    return new Observable<GeolocationCoordinates>((observer) => {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          observer.next(position.coords);
+          observer.complete();
+        },
+        (err) => observer.error(err)
+      );
+    });
+  }
+
 }
