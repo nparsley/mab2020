8895f126487eb2842b2fedbc6d9ade78e0295446
nparsley <nickparsley5@gmail.com>
2021-02-21 20:36:15 -0700

    map coords to params

--- landing/src/app/weather/forecast.service.ts
+++ landing/src/app/weather/forecast.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map} from 'rxjs/operators';
+import { HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +9,18 @@ import { Observable } from 'rxjs';
 export class ForecastService {
 
   constructor() { }
+
+  getForecast() {
+    return this.getCurrentLocation().pipe(
+      map(coords => {
+        return new HttpParams()
+        .set('lat', String(coords.latitude))
+        .set('lon', String(coords.longitude))
+        .set('units', 'imperial')
+        .set('appid', 'ad6696a544b579ee32bf7f488cd8a8b4')
+      })
+    )
+  }
 
   getCurrentLocation() {
     return new Observable<GeolocationCoordinates>((observer) => {
