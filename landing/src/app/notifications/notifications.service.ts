c83aaf4819680b5fe67c902649a628a9650dba41
nparsley <nickparsley5@gmail.com>
2021-02-25 18:36:43 -0700

    notificationsservice-command setup

--- landing/src/app/notifications/notifications.service.ts
+++ landing/src/app/notifications/notifications.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { type } from 'os';
+import { Subject } from 'rxjs';
+
+interface Command {
+  id: number;
+  type: 'success' | 'error' | 'clear';
+  text?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
+  messages: Subject<Command>;
 
-  constructor() { }
+  constructor() {
+    this.messages = new Subject<Command>();
+  }
+
+  addSuccess(message: string) {
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'success'
+    });
+  }
+
+  addError(message: string) {
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'error'
+    });
+  }
+
+  clearMessage(id: number) {
+    this.messages.next({
+      id,
+      type: 'clear'
+    });
+  }
+
+  private randomId() {
+    return Math.round(Math.random() * 10000);
+  }
+
 }
